72b75b46fc0af494967b83a2dc5817782cd39a92
samuel <samsubas01@gmail.com>
2021-07-31 17:39:49 +0300

    fixed the layout

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import navStyles from '../styles/navbar.module.css';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Nav = () => {
+
+  const [isOpen, setIsOpen] = useState('false');
+
+  const iconVariants = {
+    opened: {
+      
+    },
+    closed: {
+      
+    }
+  }
+
   return (
-    <nav className={navStyles.navbar}>
+    <nav className={navStyles.navbar} >
       <h1 className={navStyles.logo}>Efarmogi Computers</h1>
       <ul>
         <li><Link href="/">Αρχική</Link></li>
@@ -12,6 +25,9 @@ const Nav = () => {
         <li><Link href="/services">Υπηρεσίες</Link></li>
         <li><Link href="/contact">Επικοινωνία</Link></li>
       </ul>
+      <div className={navStyles.burgerContainer}>
+        <motion.div initial={false} variants={iconVariants} animate={isOpen ? 'opened' : 'closed'} onClick={() => setIsOpen(state => !state)} className={navStyles.burgerIcon} ></motion.div>
+      </div>
     </nav>
   )
 }
