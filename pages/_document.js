92d4222f99bfc31f18ca2f554880b5f3c4d1f375
samuel <samsubas01@gmail.com>
2021-08-17 21:32:02 +0300

    removed font

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="gr">
         <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" /> */}
           <meta charSet="utf-8"/>
           <meta name="description" content="Website for it support in lesvos mytilene"/>
           <meta name="viewport" content="width=device-width"/>
