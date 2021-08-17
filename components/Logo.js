f38f86e6bcb78b67b66a2bfb1b2c2b250818a056
samuel <samsubas01@gmail.com>
2021-08-17 19:43:30 +0300

    used nextjs font optimization

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from '../styles/Logo.module.css'
+import Head from 'next/head'
 
 export const Logo = () => {
   return (
     <div>
-      <h2 className={styles.logo}>Efarmogi Computers</h2>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet" />
+      </Head>
+      <h2 className={styles.logo} >Efarmogi Computers</h2>
     </div>
   )
 }
