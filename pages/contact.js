269a881e415a70d45a3f963e0d15ac16476c2786
samuel <samsubas01@gmail.com>
2021-08-19 21:50:20 +0300

    added map location

--- pages/contact.js
+++ pages/contact.js
@@ -10,6 +10,13 @@ export const contact = () => {
       <h2 className={styles.mainHeading} id="contact">Επικοινωνία</h2>
       <h3 className={styles.location}><Image src={location} alt="location" width={25} height={25}/> Τοποθεσία</h3>
       <p>Πλατεία Κωνσταντινουπόλεως 6 & Μάνου Κατράκη 1, 81101 Μυτιλήνη</p>
+      <iframe style={{
+        width: '80vw',
+        height: '60vh'
+      }}
+        frameBorder="0"
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
+      </iframe>
       <h3 className={styles.email}><Image src={email} alt="email contact" width={25} height={25}/> E-mail</h3>
       <p>efarmogi.taxidis@gmail.com</p>
       <h3 className={styles.phone}><Image src={phone} alt="phone contact" width={25} height={25}/> Τηλέφωνο Επικοινωνίας</h3>
