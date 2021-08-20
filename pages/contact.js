b6fba72d1c3e16857333f332018b000e9ac46871
samuel <samsubas01@gmail.com>
2021-08-20 17:55:37 +0300

    testing enviroment variables

--- pages/contact.js
+++ pages/contact.js
@@ -15,7 +15,7 @@ export const contact = () => {
         height: '60vh'
       }}
         frameBorder="0"
-        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.production.local.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
       </iframe>
       <h3 className={styles.email}><Image src={email} alt="email contact" width={25} height={25}/> E-mail</h3>
       <p>efarmogi.taxidis@gmail.com</p>
