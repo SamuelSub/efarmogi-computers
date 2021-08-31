60be8a749919c445241d84c3c5a14b7581028a21
samuel <samsubas01@gmail.com>
2021-08-31 22:16:26 +0300

    added componentdidmount on ifram

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,21 @@
-const Map = () => {
-  return (
-    <iframe title="map"
-      frameBorder="0"
-      loading="lazy"
-      src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
-    </iframe> 
-  )
+import React, { Component } from 'react'
+
+export default class Map extends Component {
+
+  componentDidMount() {
+    setTimeout(() => {
+      document.querySelector('iframe').style.display = 'block';
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <iframe style={{display: 'none'}} title="map"
+        frameBorder="0"
+        loading="lazy"
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
+      </iframe> 
+    )
+  }
 }
 
-export default Map
