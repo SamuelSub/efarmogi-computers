0afbda4250fd1e818cf84ea34b58e1de32406acf
samuel <samsubas01@gmail.com>
2021-08-31 23:15:55 +0300

    testing

--- components/Map.js
+++ components/Map.js
@@ -2,11 +2,12 @@
 const Map = () => {
 
   return (
-    <iframe title="map"
-      frameBorder="0"
-      loading="lazy"
-      src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
-    </iframe> 
+    // <iframe title="map"
+    //   frameBorder="0"
+    //   loading="lazy"
+    //   src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
+    // </iframe> 
+    <div></div>
   )
 }
 
