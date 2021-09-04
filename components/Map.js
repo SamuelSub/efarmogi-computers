14ad09b6f3e6a0522f07c1d8c3d8bc4fcff61291
samuel <samsubas01@gmail.com>
2021-09-04 12:35:34 +0300

    map renders after the dom is loaded

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react"
 
 const Map = () => {
 
+  let [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
+
   return (
-    <iframe title="map"
-      frameBorder="0"
-      loading="lazy"
-      src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
-    </iframe> 
+    <div>
+      {isLoaded && (
+        <iframe title="map"
+          frameBorder="0"
+          loading="lazy"
+          src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}allowFullScreen>
+        </iframe>
+      )}
+    </div>
+     
   )
 }
 
