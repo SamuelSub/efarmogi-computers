d94aa92432da905caa1cc19312e6d8149f67e643
samuel <samsubas01@gmail.com>
2021-09-05 13:32:13 +0300

    added map img that redirects to google maps

--- components/Map.js
+++ components/Map.js
@@ -1,21 +1,17 @@
-import { useEffect, useState } from "react"
 import Script from 'next/script'
+import Image from 'next/image'
+import mapImg from '../public/map-img.png'
+import styles from '../styles/Map.module.css'
+import Link from 'next/link'
 
 const Map = () => {
 
-  const [link, setLink] = useState(null);
-
-  useEffect(() => {
-    const map = <Script src={`https://www.google.com/maps/embed/v1/place?key=${process.env.API_KEY}&q=Efarm0g1+Computers`}/>
-    setLink(map.props.src)
-  },[]);
-
+  const link = <Script src={"https://goo.gl/maps/qR2Q6F38DoecmJNTA"}/>;
+    
   return (
-    <iframe title="map"
-      frameBorder="0"
-      loading="lazy"
-      src={link}allowFullScreen>
-    </iframe> 
+    <Link href={link.props.src} passHref>
+      <a target="_blank" className={styles.mapImg}><Image src={mapImg} alt="map image" quality={100}/></a>
+    </Link>
   )
 }
 
