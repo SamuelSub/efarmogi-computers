3df93947035f5c6acd068c427a6afb62760bbb31
samuel <samsubas01@gmail.com>
2021-09-05 17:56:27 +0300

    remove brackets from script src

--- components/Map.js
+++ components/Map.js
@@ -7,7 +7,7 @@ const Map = () => {
   const [mapLink, setMapLink] = useState(null);
 
   useEffect(() => {
-    const map = <Script src={'https://www.openstreetmap.org/export/embed.html?bbox=26.554936766624454%2C39.102005672047795%2C26.557618975639347%2C39.10346684144032&amp;layer=mapnik&amp;marker=39.10273626052978%2C26.556277871131897'}/>;
+    const map = <Script src='https://www.openstreetmap.org/export/embed.html?bbox=26.554936766624454%2C39.102005672047795%2C26.557618975639347%2C39.10346684144032&amp;layer=mapnik&amp;marker=39.10273626052978%2C26.556277871131897'/>;
     setMapLink(map.props.src)
   }, [])
     
